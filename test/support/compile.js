855e001c45622ce9ad0960b932cb9bf2be5c3853
jonschlinkert <jon.schlinkert@sellside.com>
2017-10-21 14:29:02 -0400

    add `raw` helper

--- test/support/compile.js
+++ test/support/compile.js
@@ -5,15 +5,18 @@ var write = require('write');
 var hbs = require('handlebars');
 
 hbs.registerHelper('helperMissing', function() {})
+hbs.registerHelper('raw', function(options) {
+  return options.fn();
+});
 
 function expected(pattern, options) {
   var opts = Object.assign({cwd: path.join(__dirname, '../expected')}, options);
   for (let file of glob.sync(pattern, opts)) {
     var fp = path.join(opts.cwd, file);
-    console.log(fp)
     var str = fs.readFileSync(fp, 'utf8');
     var fn = hbs.compile(str);
     fn();
+
   }
 };
 
